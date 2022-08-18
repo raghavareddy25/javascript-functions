ca171164c6c4e12a5d30bb298c2caeae30f30fe3
raghavareddy <raghavareddy2325@gmail.com>
2022-08-18 21:21:27 +0530

    code added

---
+++ functions.js
@@ -0,0 +1,22 @@
+// declaring a function
+// function with parameters
+function greet(name) {
+    console.log("Hello " + name + ":)");
+}
+
+// variable name can be different
+let name = ("raghava");
+
+// calling function
+greet(name);
+
+
+// program to add two numbers using a function
+// declaring a function
+function add(a, b) {
+    console.log(a + b);
+}
+
+// calling functions
+add(3,4);
+add(2,9);
